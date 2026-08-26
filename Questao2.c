6ef9e96f7a694b618e455fcd9266278009d1cee0
Áurea Patrícia Lima Oliveira De Souza <patriciaaurea283@gmail.com>
2026-08-25 21:54:19 -0300

    Delete Questao2.c

--- Questao2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    float euler = 2.71828;
-    printf("O valor de Euler formatado eh: %.3f\n", euler);
-    system("PAUSE");
-    return 0;
-}
