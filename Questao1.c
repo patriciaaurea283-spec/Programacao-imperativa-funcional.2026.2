a7397d72ac7a7f6770df40a20dddc1329ecb1a4c
Áurea Patrícia Lima Oliveira De Souza <patriciaaurea283@gmail.com>
2026-08-25 21:54:02 -0300

    Delete Questao1.c

--- Questao1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int ano_letivo = 2026;
-    printf("O ano letivo corrente eh: %d\n", ano_letivo);
-    system("PAUSE");
-    return 0;
-}
